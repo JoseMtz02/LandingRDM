7e0e385b0e41344946a06dcf1693606b8f92b202
José Martínez <110182478+JoseMtz02@users.noreply.github.com>
2025-05-28 18:24:33 -0500

    feat: cargando imagenes de carrusel

--- src/views/section-sliders/SlidersSection.tsx
+++ src/views/section-sliders/SlidersSection.tsx
@@ -14,15 +14,67 @@ const SlidersPage: React.FC = () => {
     { id: 10, src: '/images/ArtesaniasMonte.jpg', alt: 'Artesianias Monte', label: 'Compra artesanía y productos regionales' },
   ];
 
+  const festsImages = [
+    { id: 1, src: '/images/fests/Carnaval.jpg', alt: 'Carnaval', label: 'Carnaval de la Montaña' },
+    { id: 2, src: '/images/fests/Fuegos.jpg', alt: 'Fuegos', label: 'Fuegos Artificiales' },
+    { id: 3, src: '/images/fests/Hiloche.jpg', alt: 'Hiloche', label: 'Hiloche bailable' },
+    { id: 4, src: '/images/fests/Informacion.jpg', alt: 'Festividad', label: 'Santo Patrono' },
+    { id: 5, src: '/images/fests/Paisaje.jpg', alt: 'Pueblo', label: 'Pueblo Mágico' },
+    { id: 6, src: '/images/fests/Paste.jpg', alt: 'Paste', label: 'Festival Paste' },
+    { id: 7, src: '/images/fests/Plata.jpg', alt: 'Joyeria', label: 'Exposición Joyeria Plata' },
+    { id: 8, src: '/images/fests/Plata2.jpg', alt: 'Plata', label: 'Exposición Plata' },
+    { id: 9, src: '/images/fests/SSanta.jpg', alt: 'Semana', label: 'Semana Santa' },
+    { id: 10, src: '/images/fests/Zelontla.jpeg', alt: 'Zelontla', label: 'Zelontla' },
+  ];
+
+  const hotelesImages = [
+    { id: 1, src: '/images/hoteles/Cabañas.jpg', alt: 'Carnaval', label: 'Carnaval de la Montaña' },
+    { id: 2, src: '/images/hoteles/Camas.jpg', alt: 'Fuegos', label: 'Fuegos Artificiales' },
+    { id: 3, src: '/images/hoteles/HotelBoutique.jpg', alt: 'Hiloche', label: 'Hiloche bailable' },
+    { id: 4, src: '/images/hoteles/HotelParaisoReal.jpg', alt: 'Festividad', label: 'Santo Patrono' },
+    { id: 5, src: '/images/hoteles/HotelPosadaInterior.jpg', alt: 'Pueblo', label: 'Pueblo Mágico' },
+    { id: 6, src: '/images/hoteles/HotelPosadaReal.jpg', alt: 'Paste', label: 'Festival Paste' },
+    { id: 7, src: '/images/hoteles/MineraldelMonte.jpg', alt: 'Joyeria', label: 'Exposición Joyeria Plata' },
+    { id: 8, src: '/images/hoteles/MineralMonteInterior.jpg', alt: 'Plata', label: 'Exposición Plata' },
+    { id: 9, src: '/images/hoteles/PanoramicaRM.jpg', alt: 'Semana', label: 'Semana Santa' },
+    { id: 10, src: '/images/hoteles/SpaHolistico.jpg', alt: 'Zelontla', label: 'Zelontla' },
+  ];
+
+  const restaurantsImages = [
+    { id: 1, src: '/images/restaurants/Restaurante01.jpg', alt: 'Carnaval', label: 'Carnaval de la Montaña' },
+    { id: 2, src: '/images/restaurants/02.jpg', alt: 'Fuegos', label: 'Fuegos Artificiales' },
+    { id: 3, src: '/images/restaurants/03.jpg', alt: 'Hiloche', label: 'Hiloche bailable' },
+    { id: 4, src: '/images/restaurants/04.jpg', alt: 'Festividad', label: 'Santo Patrono' },
+    { id: 5, src: '/images/restaurants/05.jpg', alt: 'Pueblo', label: 'Pueblo Mágico' },
+    { id: 6, src: '/images/restaurants/06.jpg', alt: 'Paste', label: 'Festival Paste' },
+    { id: 7, src: '/images/restaurants/07.jpg', alt: 'Joyeria', label: 'Exposición Joyeria Plata' },
+    { id: 8, src: '/images/restaurants/08.jpg', alt: 'Plata', label: 'Exposición Plata' },
+    { id: 9, src: '/images/restaurants/09.jpg', alt: 'Semana', label: 'Semana Santa' },
+    { id: 10, src: '/images/restaurants/10.jpg', alt: 'Zelontla', label: 'Zelontla' },
+  ];
+
+  const toursImages = [
+    { id: 1, src: '/images/tours/1primasmas_basalticos.jpg', alt: 'Carnaval', label: 'Carnaval de la Montaña' },
+    { id: 2, src: '/images/tours/caption.jpg', alt: 'Fuegos', label: 'Fuegos Artificiales' },
+    { id: 3, src: '/images/tours/panteon-ingles.jpg', alt: 'Hiloche', label: 'Hiloche bailable' },
+    { id: 4, src: '/images/tours/prismas-cascada.jpg', alt: 'Festividad', label: 'Santo Patrono' },
+    { id: 5, src: '/images/tours/prismas1.jpg', alt: 'Pueblo', label: 'Pueblo Mágico' },
+    { id: 6, src: '/images/tours/real-del-monte2.jpg', alt: 'Paste', label: 'Festival Paste' },
+    { id: 7, src: '/images/tours/real-del-monte.jpg', alt: 'Joyeria', label: 'Exposición Joyeria Plata' },
+    { id: 8, src: '/images/tours/tm_museos.jpg', alt: 'Plata', label: 'Exposición Plata' },
+    { id: 9, src: '/images/tours/tranvia-real.jpg', alt: 'Semana', label: 'Semana Santa' },
+    { id: 10, src: '/images/tours/turibus-realdelmonte.jpg', alt: 'Zelontla', label: 'Zelontla' },
+  ];
+
   return (
-      <div className="bg-gradient-to-r from-black via-gray-800 via-105% to-gray-500 top-10% to-black relative">
+      <div className="bg-gradient-to-r from-black via-black-900 via-105% to-black-500 top-10% to-black relative">
           <div className="absolute inset-y-0 left-0 w-200 bg-gradient-to-r from-black to-transparent z-10 pointer-events-none"></div>
           <div className="absolute inset-y-0 right-0 w-24 bg-gradient-to-l from-black to-transparent z-10 pointer-events-none"></div>
           
           <section className="mb-16 relative">
         <FadeSlider
           title="Festividades"
-          images={sharedImages}
+          images={festsImages}
           spaceBetween={60}
           slidesPerView={3}
           speed={600}
@@ -42,7 +94,7 @@ const SlidersPage: React.FC = () => {
       <section className="mb-16 relative">
         <FadeSlider
           title="Hoteles"
-          images={sharedImages}
+          images={hotelesImages}
           spaceBetween={60}
           slidesPerView={3}
           speed={600}
@@ -52,7 +104,7 @@ const SlidersPage: React.FC = () => {
       <section className="mb-16 relative">
         <FadeSlider
           title="Restaurantes"
-          images={sharedImages}
+          images={restaurantsImages}
           spaceBetween={60}
           slidesPerView={3}
           speed={600}
@@ -61,8 +113,8 @@ const SlidersPage: React.FC = () => {
 
       <section className="relative">
         <FadeSlider
-          title="Hoteles"
-          images={sharedImages}
+          title="Tours"
+          images={toursImages}
           spaceBetween={60}
           slidesPerView={3}
           speed={600}
